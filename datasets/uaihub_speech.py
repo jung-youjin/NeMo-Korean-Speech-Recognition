2a07dd720c8560cd40a0e05b40d5f25cc7f7ab61
YouJin <jungyoujin0527@gmail.com>
2021-07-20 08:12:44 +0000

    Test pickle dump load

--- datasets/uaihub_speech.py
+++ datasets/uaihub_speech.py
@@ -10,15 +10,15 @@ import pickle
 #with open('data/aihub/char2id.pkl', 'rb') as f:
 #    char2id = pickle.load(f)
 
-folder_path = "/content/korean-speech-recognition-quartznet/datasets/meta/aihub/"
+folder_path = "/content/korean-speech-recognition-quartznet/datasets/meta/aihub/pickle/"
     
-with open(folder+path+'id2char.pkl', 'rb') as f:
+with open(folder_path+'id2char.pkl', 'rb') as f:
     vocab = ''.join(pickle.load(f)[:991])
 
-with open(folder+path+'train_data.pkl', 'rb') as f:
+with open(folder_path+'train_data.pkl', 'rb') as f:
     train_data = pickle.load(f)
     
-with open(folder+path+'test_data.pkl', 'rb') as f:
+with open(folder_path+'test_data.pkl', 'rb') as f:
     test_data = pickle.load(f)
     
 #PAD_TOKEN = int(char2id['_'])
